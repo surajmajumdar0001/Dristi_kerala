2876b40ba0c29932bc6f005ecfe5d80756288366
prasanna2400 <prasannajeet.v@beehyv.com>
2024-04-22 16:40:13 +0530

    dummy method implementation for scheduleHearing and updateScheduledHearing methods

--- backend/scheduler-svc/src/main/java/digit/service/HearingService.java
+++ backend/scheduler-svc/src/main/java/digit/service/HearingService.java
@@ -1,14 +1,7 @@
 package digit.service;
 
 
-import digit.config.Configuration;
-import digit.enrichment.HearingEnrichment;
-import digit.kafka.Producer;
-import digit.repository.HearingRepository;
-import digit.validator.HearingValidator;
-import digit.web.models.*;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import digit.web.models.ScheduleHearing;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,64 +10,27 @@ import java.util.List;
 public class HearingService {
 
 
-    private final HearingValidator hearingValidator;
 
-    private final HearingEnrichment hearingEnrichment;
-
-    private final Producer producer;
-
-    private final Configuration config;
-    @Autowired
-    private HearingRepository hearingRepository;
-
-
-    @Autowired
-    public HearingService(HearingValidator hearingValidator, HearingEnrichment hearingEnrichment, Producer producer, Configuration config) {
-        this.hearingValidator = hearingValidator;
-        this.hearingEnrichment = hearingEnrichment;
-        this.producer = producer;
-        this.config = config;
-    }
-
-
-    public List<ScheduleHearing> schedule(ScheduleHearingRequest schedulingRequests) {
+    public void scheduleHearing (List<ScheduleHearing> schedulingRequests){
 
         //validate hearing request here
-        hearingValidator.validateHearing(schedulingRequests.getHearing());
+
 
         // enhance the hearing request here
-        hearingEnrichment.enrichScheduleHearing(schedulingRequests.getRequestInfo(), schedulingRequests.getHearing());
+
+        //1.0 id gen 2.0 audit details
+
 
         //push to kafka
-        producer.push(config.getScheduleHearingTopic(), schedulingRequests.getHearing());
-
-        return schedulingRequests.getHearing();
     }
 
     // to update the status of existing hearing to reschedule
-    public  List<ScheduleHearing> update(ScheduleHearingRequest scheduleHearingRequest) {
+    private void updateScheduledHearing(List<ScheduleHearing> updateScheduleHearingRequest){
+        // validate request
 
-        hearingValidator.validateHearingOnUpdate(scheduleHearingRequest);
+        //enhance the request
+        // updateStatus and audit details
 
-        hearingEnrichment.enrichUpdateScheduleHearing(scheduleHearingRequest.getRequestInfo(), scheduleHearingRequest.getHearing());
-
-        producer.push(config.getScheduleHearingUpdateTopic(), scheduleHearingRequest.getHearing());
-
-        return scheduleHearingRequest.getHearing();
-
+        //push to kafka
     }
-
-    public List<ScheduleHearing> search(HearingSearchRequest request) {
-
-        return hearingRepository.getHearings(request.getCriteria());
-
-    }
-
-
-    public List<AvailabilityDTO> getAvailableDateForHearing(HearingSearchCriteria hearingSearchCriteria) {
-
-        return hearingRepository.getAvailableDatesOfJudges(hearingSearchCriteria);
-    }
-
-
 }
