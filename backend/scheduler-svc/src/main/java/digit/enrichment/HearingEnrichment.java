be8e45c62c5b63cbe4f725305ea410da410bbf02
prasanna2400 <prasannajeet.v@beehyv.com>
2024-04-22 21:10:48 +0530

    added enrichment and validator clazz

--- backend/scheduler-svc/src/main/java/digit/enrichment/HearingEnrichment.java
+++ backend/scheduler-svc/src/main/java/digit/enrichment/HearingEnrichment.java
@@ -2,12 +2,11 @@ package digit.enrichment;
 
 
 import digit.config.Configuration;
-import digit.models.coremodels.AuditDetails;
 import digit.util.IdgenUtil;
 import digit.web.models.ScheduleHearing;
-import digit.web.models.enums.Status;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.common.contract.request.RequestInfo;
+import org.egov.tracer.model.AuditDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,39 +23,27 @@ public class HearingEnrichment {
     private Configuration configuration;
 
 
-    public void enrichScheduleHearing(RequestInfo requestInfo, List<ScheduleHearing> hearingList) {
+    public void enrichForScheduleHearing(RequestInfo requestInfo, List<ScheduleHearing> hearingList) {
 
         log.info("starting update method for schedule hearing enrichment");
         log.info("generating IDs for schedule hearing enrichment using IdGenService");
-        List<String> idList = idgenUtil.getIdList(requestInfo, hearingList.get(0).getTenantId(), configuration.getHearingIdFormat(), null, hearingList.size());
-        AuditDetails auditDetails = getAuditDetailsScheduleHearing(requestInfo);
+        List<String> idList = idgenUtil.getIdList(requestInfo,
+                hearingList.get(0).getTenantId(),
+                configuration.getHearingIdFormat(), null, hearingList.size());
         int index = 0;
         for (ScheduleHearing hearing : hearingList) {
+            AuditDetails auditDetails = getAuditDetailsScheduleHearing(requestInfo);
             hearing.setAuditDetails(auditDetails);
             hearing.setHearingBookingId(idList.get(index++));
-            hearing.setRowVersion(1);
-            hearing.setStatus(Status.SCHEDULED);
         }
 
-    }
-
-
-    public void enrichUpdateScheduleHearing(RequestInfo requestInfo, List<ScheduleHearing> hearingList) {
-
-        hearingList.stream().forEach((hearing) -> {
-
-            Long currentTime = System.currentTimeMillis();
-            hearing.getAuditDetails().setLastModifiedTime(currentTime);
-            hearing.getAuditDetails().setLastModifiedBy(requestInfo.getUserInfo().getUuid());
-            hearing.setRowVersion(hearing.getRowVersion() + 1);
-
-        });
 
     }
 
     private AuditDetails getAuditDetailsScheduleHearing(RequestInfo requestInfo) {
 
-        return AuditDetails.builder().createdBy(requestInfo.getUserInfo().getUuid()).createdTime(System.currentTimeMillis()).lastModifiedBy(requestInfo.getUserInfo().getUuid()).lastModifiedTime(System.currentTimeMillis()).build();
-
+        AuditDetails auditDetails = AuditDetails.builder()
+                .build();
+        return auditDetails;
     }
 }
--- backend/scheduler-svc/src/main/java/digit/enrichment/JudgeCalendarEnrichment.java
+++ backend/scheduler-svc/src/main/java/digit/enrichment/JudgeCalendarEnrichment.java
@@ -1,36 +1,23 @@
 package digit.enrichment;
 
-import digit.models.coremodels.AuditDetails;
-import digit.web.models.JudgeCalendarRule;
+
+import digit.util.IdgenUtil;
+import digit.web.models.ScheduleHearing;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.common.contract.request.RequestInfo;
+import org.egov.tracer.model.AuditDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.UUID;
 
 @Component
 @Slf4j
 public class JudgeCalendarEnrichment {
 
+    @Autowired
+    private IdgenUtil idgenUtil;
 
-    public void enrichUpdateJudgeCalendar(RequestInfo requestInfo, List<JudgeCalendarRule> judgeCalendarRule) {
-        AuditDetails auditDetails = getAuditDetailsScheduleHearing(requestInfo);
-
-        judgeCalendarRule.forEach((calendar) -> {
-
-            calendar.setId(UUID.randomUUID().toString());
-            calendar.setAuditDetails(auditDetails);
-            calendar.setRowVersion(1);
-
-        });
-    }
-
-    private AuditDetails getAuditDetailsScheduleHearing(RequestInfo requestInfo) {
-
-        return AuditDetails.builder().createdBy(requestInfo.getUserInfo().getUuid()).createdTime(System.currentTimeMillis()).lastModifiedBy(requestInfo.getUserInfo().getUuid()).lastModifiedTime(System.currentTimeMillis()).build();
-
-    }
 
 
 }
