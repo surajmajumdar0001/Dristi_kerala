198e2ec48198ef1e326eba9cc790c76feea5c328
prasanna2400 <prasannajeet.v@beehyv.com>
2024-05-10 20:43:08 +0530

    clean db.flyway files

--- backend/scheduler-svc/src/main/resources/db/migration/main/V20240507161100__ut_update_tables_ddl.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE hearing_booking ALTER COLUMN date TYPE VARCHAR(64);
-
-ALTER TABLE hearing_booking ALTER COLUMN start_time TYPE VARCHAR(64);
-
-ALTER TABLE hearing_booking ALTER COLUMN end_time TYPE VARCHAR(64);
