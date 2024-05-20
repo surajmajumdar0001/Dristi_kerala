ae63beabd74993f32930ec57cf35acd907cf704c
prasanna2400 <prasannajeet.v@beehyv.com>
2024-05-20 16:32:29 +0530

    added log in controller

--- backend/scheduler-svc/src/main/java/digit/web/controllers/HearingApiController.java
+++ backend/scheduler-svc/src/main/java/digit/web/controllers/HearingApiController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,29 +21,34 @@ import java.util.List;
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-15T13:15:39.759211883+05:30[Asia/Kolkata]")
 @RestController("hearingApiController")
 @RequestMapping("")
+@Slf4j
 public class HearingApiController {
 
+    private final HearingService hearingService;
+
     @Autowired
-    private HearingService hearingService;
+    public HearingApiController(HearingService hearingService) {
+        this.hearingService = hearingService;
+    }
 
 
     @RequestMapping(value = "/hearing/v1/_schedule", method = RequestMethod.POST)
     public ResponseEntity<HearingResponse> scheduleHearing(@Parameter(in = ParameterIn.DEFAULT, description = "Hearing Details and Request Info", required = true, schema = @Schema()) @Valid @RequestBody ScheduleHearingRequest request) {
-        //service call
+        log.info("api=/hearing/v1/_schedule, result = IN_PROGRESS");
         List<ScheduleHearing> scheduledHearings = hearingService.schedule(request);
         HearingResponse response = HearingResponse.builder().hearings(scheduledHearings).responseInfo(ResponseInfoFactory.createResponseInfo(request.getRequestInfo(), true)).build();
+        log.info("api=/hearing/v1/_schedule, result = SUCCESS");
         return ResponseEntity.accepted().body(response);
     }
 
 
     @RequestMapping(value = "/hearing/v1/_search", method = RequestMethod.POST)
     public ResponseEntity<HearingResponse> searchHearing(@Parameter(in = ParameterIn.DEFAULT, description = "Hearing Details and Request Info", required = true, schema = @Schema()) @Valid @RequestBody HearingSearchRequest request) {
-
+        log.info("api=/hearing/v1/_search, result = IN_PROGRESS");
         List<ScheduleHearing> scheduledHearings = hearingService.search(request);
-
         HearingResponse response = HearingResponse.builder().responseInfo(ResponseInfoFactory.createResponseInfo(request.getRequestInfo(), true))
                 .hearings(scheduledHearings).build();
-
+        log.info("api=/hearing/v1/_search, result = SUCCESS");
         return ResponseEntity.accepted().body(response);
     }
 
