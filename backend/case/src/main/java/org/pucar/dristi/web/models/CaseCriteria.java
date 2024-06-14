5c972a523fc72017ef687522a1c309646226a9e4
rkum113 <raushan.k@beehyv.com>
2024-06-14 12:14:37 +0530

    adding e-filling backend

---
+++ backend/case/src/main/java/org/pucar/dristi/web/models/CaseCriteria.java
@@ -0,0 +1,65 @@
+package org.pucar.dristi.web.models;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.validation.annotation.Validated;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
+/**
+ * CaseCriteria
+ */
+@Validated
+@jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-15T11:31:40.281899+05:30[Asia/Kolkata]")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CaseCriteria {
+    @JsonProperty("caseId")
+    private String caseId = null;
+
+    @JsonProperty("cnrNumber")
+    private String cnrNumber = null;
+
+    @JsonProperty("filingNumber")
+    private String filingNumber = null;
+
+    @JsonProperty("courtCaseNumber")
+    private String courtCaseNumber = null;
+
+    @JsonProperty("filingFromDate")
+    @Valid
+    private LocalDate filingFromDate = null;
+
+    @JsonProperty("filingToDate")
+    @Valid
+    private LocalDate filingToDate = null;
+
+    @JsonProperty("registrationFromDate")
+    @Valid
+    private LocalDate registrationFromDate = null;
+
+    @JsonProperty("registrationToDate")
+    @Valid
+    private LocalDate registrationToDate = null;
+
+    @JsonProperty("responseList")
+    @Valid
+    private List<CourtCase> responseList = null;
+
+    public CaseCriteria addResponseListItem(CourtCase responseListItem) {
+        if (this.responseList == null) {
+            this.responseList = new ArrayList<>();
+        }
+        this.responseList.add(responseListItem);
+        return this;
+    }
+
+}
