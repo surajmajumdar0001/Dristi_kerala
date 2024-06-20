0920f3eaa23460536ca5ce5c878c624c6a523306
Ajith-beehyv <ajith.k@beehyv.com>
2024-06-20 17:21:24 +0530

    update summons end point fix

--- backend/summons-svc/src/main/java/digit/web/controllers/SummonsApiController.java
+++ backend/summons-svc/src/main/java/digit/web/controllers/SummonsApiController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Collections;
 
 @jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-05-29T13:38:04.562296+05:30[Asia/Calcutta]")
 @Controller
@@ -50,7 +49,7 @@ public class SummonsApiController {
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
-    @RequestMapping(value = "summons/v1/_/updateSummons", method = RequestMethod.POST)
+    @RequestMapping(value = "summons/v1/_updateSummons", method = RequestMethod.POST)
     public ResponseEntity<UpdateSummonsResponse> updateSummonsStatus(@Parameter(in = ParameterIn.DEFAULT, description = "Details for the Updating Summons + RequestInfo meta data.", required = true, schema = @Schema()) @Valid @RequestBody UpdateSummonsRequest request) {
         ResponseInfo responseInfo = responseInfoFactory.createResponseInfoFromRequestInfo(request.getRequestInfo(), true);
         ChannelMessage channelMessage = summonsService.updateSummonsDeliveryStatus(request);
