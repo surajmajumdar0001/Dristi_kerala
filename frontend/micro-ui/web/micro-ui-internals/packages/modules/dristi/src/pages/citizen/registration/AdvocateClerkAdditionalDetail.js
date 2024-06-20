dbeb65be11ef25aa8645fdcd79d14cbf99078be6
shambhavigoel1002 <shambhavi.g@beehyv.com>
2024-06-20 16:24:52 +0530

    disable button

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/registration/AdvocateClerkAdditionalDetail.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/pages/citizen/registration/AdvocateClerkAdditionalDetail.js
@@ -80,6 +80,9 @@ function AdvocateClerkAdditionalDetail({ params, setParams, path, config, pathOn
         if (Array.isArray(formData[curr.body[0].key][input.name]) && formData[curr.body[0].key][input.name].length === 0) {
           isDisabled = true;
         }
+        if (formData?.clientDetails?.barRegistrationNumber?.length < 8) {
+          isDisabled = true;
+        }
       });
     });
     if (isDisabled) {
