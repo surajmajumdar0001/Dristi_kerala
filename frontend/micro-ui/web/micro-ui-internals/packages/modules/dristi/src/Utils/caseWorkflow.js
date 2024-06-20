23362d89b925db6ae4a8dfa3e6043bc952900071
rkum113 <raushan.k@beehyv.com>
2024-06-20 18:14:20 +0530

    frontend e-filling

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/Utils/caseWorkflow.js
@@ -0,0 +1,9 @@
+export const CaseWorkflowState = {
+  CASE_RE_ASSIGNED: "CASE_RE_ASSIGNED",
+  DRAFT_IN_PROGRESS: "DRAFT_IN_PROGRESS",
+  UNDER_SCRUTINY: "UNDER_SCRUTINY",
+};
+
+export const CaseWorkflowAction = {
+  EDIT_CASE: "EDIT_CASE",
+};
