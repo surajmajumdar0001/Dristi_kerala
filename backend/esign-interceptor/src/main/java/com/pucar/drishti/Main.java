e6fe21ddd300b5c31fb632613908a51b3efe92d8
prasanna2400 <prasannajeet.v@beehyv.com>
2024-07-02 19:16:14 +0530

    excluded data source auto config

--- backend/esign-interceptor/src/main/java/com/pucar/drishti/Main.java
+++ backend/esign-interceptor/src/main/java/com/pucar/drishti/Main.java
@@ -4,11 +4,12 @@ package com.pucar.drishti;
 import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @Import({TracerConfiguration.class})
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.pucar.drishti", "com.pucar.drishti.web.controllers", "com.pucar.drishti.config"})
 public class Main {
 
