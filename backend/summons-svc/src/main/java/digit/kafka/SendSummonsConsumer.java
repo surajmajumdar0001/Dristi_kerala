99d7f31052e63e191e7b7ecdfc9e2efb328e1d1a
Ajithjaya <ajith30022@gmail.com>
2024-07-04 11:30:46 +0530

    new files for task changes

---
+++ backend/summons-svc/src/main/java/digit/kafka/SendSummonsConsumer.java
@@ -0,0 +1,43 @@
+package digit.kafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import digit.service.SummonsService;
+import digit.web.models.TaskRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+@Slf4j
+@EnableAsync
+public class SendSummonsConsumer {
+
+    private final SummonsService summonsService;
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public SendSummonsConsumer(SummonsService summonsService, ObjectMapper objectMapper) {
+        this.summonsService = summonsService;
+        this.objectMapper = objectMapper;
+    }
+
+    @KafkaListener(topics = "generate-summons-document")
+    @Async
+    public void listen(final HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        try {
+            TaskRequest taskRequest = objectMapper.convertValue(record, TaskRequest.class);
+            log.info(taskRequest.getTask().toString());
+            summonsService.sendSummonsViaChannels(taskRequest);
+        } catch (final Exception e) {
+            log.error("Error while listening to value: " + record + ": ", e);
+        }
+    }
+}
---
+++ backend/summons-svc/src/main/java/digit/web/models/TaskRequest.java
@@ -0,0 +1,39 @@
+package digit.web.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.UUID;
+
+import org.egov.common.contract.request.RequestInfo;
+
+import org.springframework.validation.annotation.Validated;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
+/**
+ * TaskRequest
+ */
+@Validated
+@jakarta.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2024-04-18T11:14:50.003326400+05:30[Asia/Calcutta]")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskRequest   {
+
+        @JsonProperty("orderId")
+        @Valid
+        private UUID orderId = null;
+
+        @JsonProperty("RequestInfo")
+        @Valid
+        private RequestInfo requestInfo = null;
+
+        @JsonProperty("task")
+        @Valid
+        private Task task = null;
+
+
+}
