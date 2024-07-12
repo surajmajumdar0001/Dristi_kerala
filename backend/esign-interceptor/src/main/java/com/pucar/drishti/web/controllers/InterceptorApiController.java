dda071971358d2dab1be9cb8a49203e1eecc09d8
prasanna2400 <prasannajeet.v@beehyv.com>
2024-07-12 12:15:21 +0530

    resolved merge conflict

--- backend/esign-interceptor/src/main/java/com/pucar/drishti/web/controllers/InterceptorApiController.java
+++ backend/esign-interceptor/src/main/java/com/pucar/drishti/web/controllers/InterceptorApiController.java
@@ -37,7 +37,6 @@ public class InterceptorApiController {
     public ResponseEntity<?> redirectHandler(@RequestParam("result") String result, @RequestParam("filestoreId") String filestoreId, @RequestParam("userType") String userType) {
         log.info("redirecting through get method");
 
-
         // Construct the final redirect URL
         String redirectUri = configs.getRedirectUrl() + userType + "/dristi";
         redirectUri += "?result=" + result + "&filestoreId=" + filestoreId;
@@ -56,13 +55,6 @@ public class InterceptorApiController {
         log.info(espId);
 
 
-        return new ResponseEntity<>(headers, HttpStatus.TEMPORARY_REDIRECT);
-    }
-
-
-
-    @RequestMapping(value = "/v1/_intercept", method = RequestMethod.POST)
-    public ModelAndView eSignV1Interceptor(@RequestParam("eSignResponse") String response, @RequestParam("espTxnID") String espId) {
         String filestoreId = "";
         String result = "error";
 
