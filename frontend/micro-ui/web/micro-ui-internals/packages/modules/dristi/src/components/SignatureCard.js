adc8f27d6577f48972b728e7bbe1a070c9c68852
shambhavigoel1002 <shambhavi.g@beehyv.com>
2024-07-13 16:59:11 +0530

    test

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/SignatureCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/SignatureCard.js
@@ -31,19 +31,21 @@ function SignatureCard({ input, data, t, index, onSelect, formData, configKey, h
   }, [configKey, name]);
   function setValue(value, input) {
     if (Array.isArray(input)) {
-      onSelect(config.key, {
-        ...formData[config.key],
+      onSelect(uploadModalConfig.key, {
+        ...formData[uploadModalConfig.key],
         ...input.reduce((res, curr) => {
           res[curr] = value[curr];
           return res;
         }, {}),
       });
-    } else onSelect(config.key, { ...formData[config.key], [input]: value });
+    } else onSelect(uploadModalConfig.key, { ...formData[uploadModalConfig.key], [input]: value });
   }
   const isSignSuccess = useMemo(() => localStorage.getItem("isSignSuccess"), []);
   console.log(isSignSuccess, "IN card");
-  if (isSignSuccess === "success") {
-    setValue(["aadharsignature"], name);
+  if (isSignSuccess) {
+    if (isSignSuccess === "success") {
+      setValue(["aadharsignature"], name);
+    }
     localStorage.removeItem("isSignSuccess");
   }
 
