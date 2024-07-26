cb123b97feb4807bc9d8ae0977009f5d99b7ca1d
Ajithjaya <ajith30022@gmail.com>
2024-07-26 11:08:20 +0530

    epost tracker api and model classes

---
+++ backend/epost-tracker/src/main/java/org/pucar/dristi/config/EPostConfiguration.java
@@ -0,0 +1,95 @@
+package org.pucar.dristi.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class EPostConfiguration {
+
+    @Value("${egov-state-level-tenant-id}")
+    private String egovStateTenantId;
+
+//    //ETreasury
+//    @Value("${treasury-public-key}")
+//    private String publicKey;
+//
+//    @Value("${treasury-client-secret}")
+//    private String clientSecret;
+//
+//    @Value("${treasury-client-id}")
+//    private String clientId;
+//
+//    @Value("${service-dept-code}")
+//    private String serviceDeptCode;
+//
+//    @Value("${office-code}")
+//    private String officeCode;
+//
+//    @Value("${dept-reference-id}")
+//    private String deptReferenceId;
+//
+//    @Value("${treasury-server-status-url}")
+//    private String serverStatusUrl;
+//
+//    @Value("${treasury-auth-url}")
+//    private String authUrl;
+//
+//    @Value("${treasury-challan-generate-url}")
+//    private String challanGenerateUrl;
+//
+//    @Value("${treasury-double-verification-url}")
+//    private String doubleVerificationUrl;
+//
+//    @Value("${treasury-print-slip-url}")
+//    private String printSlipUrl;
+//
+//    @Value("${treasury-transaction-details-url}")
+//    private String transactionDetailsUrl;
+//
+//    @Value("${treasury-refund-request-url}")
+//    private String refundRequestUrl;
+//
+//    @Value("${treasury-refund-status-url}")
+//    private String refundStatusUrl;
+//
+//    @Value("${egov.collectionservice.host}")
+//    private String collectionServiceHost;
+//
+//    @Value("${egov.collectionservice.payment.create}")
+//    private String collectionsPaymentCreatePath;
+//
+//    @Value("${egov.file.store.treasury.module}")
+//    private String treasuryFileStoreModule;
+//
+//    @Value("${egov.file.store.host}")
+//    private String fileStoreHost;
+//
+//    @Value("${egov.file.store.save.endpoint}")
+//    private String fileStoreEndPoint;
+
+    @Value("${egov.idgen.host}")
+    private String idGenHost;
+
+    @Value("${egov.idgen.path}")
+    private String idGenPath;
+
+    @Value("${egov.idgen.name}")
+    private String idName;
+
+    //Summons
+    @Value("${egov.summons.host}")
+    private String summonsHost;
+
+    @Value("${egov.summons.update.endpoint}")
+    private String summonsUpdateEndPoint;
+}
---
+++ backend/epost-tracker/src/main/java/org/pucar/dristi/model/EPostTrackerSearchRequest.java
@@ -0,0 +1,27 @@
+package org.pucar.dristi.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.egov.common.contract.request.RequestInfo;
+import org.egov.common.contract.response.ResponseInfo;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EPostTrackerSearchRequest {
+
+    @JsonProperty("RequestInfo")
+    @Valid
+    private RequestInfo requestInfo = null;
+
+    @JsonProperty("ePostTrackerSearchCriteria")
+    @Valid
+    private EPostTrackerSearchCriteria ePostTrackerSearchCriteria = null;
+}
